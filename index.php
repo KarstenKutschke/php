52c3faca786d576e49d7a3bed898018995e45417
Karsten Kutschke <karsten.kutschke@gmail.com>
2023-04-05 14:26:14 +0200

    upload after complete revision

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include('./template/navbar.php'); 
+    require_once('./app/controller/UserController.php');
+    use App\Controller\UserController;
+    session_start(); 
+?>
+
+<div class="container pt-3">
+    <h3>Welcome to all!</h3>
+    <?php
+    if (isset($_POST["deleteProfile"])) {
+        $userctr = new UserController();
+        // $userctr->updatePasswordByEmail($_SESSION['user'], $userctr->hashPassword($_POST["password"]));
+        $email = $_SESSION['user'];
+        $userctr->deleteByEmail($email);
+        echo "Ihr Profil " . $email . " wurde gelöscht.";
+    }
+    ?>
+</div>
+
+<?php include('./template/footer.php'); ?>
---
+++ login.php
@@ -0,0 +1,58 @@
+<?php 
+session_start();
+require_once('./app/controller/UserController.php');
+use App\Controller\UserController;
+$email = "";
+$error = "";
+$loginFailed = false;
+if(isset($_POST['email'])) {
+  $controller = new UserController();
+
+  $result = $controller->login($_POST['email'],$_POST['password']);
+
+  if ($controller->checkIfUserExisted($_POST['email'])) {
+    if($result) {
+      $_SESSION['user'] = $_POST['email'];
+      header('Location:profile.php');
+      die;
+    }
+    else {
+      $error = "user pass wrong";
+    }    
+  } else {
+    $loginFailed = true;
+  }
+
+}
+include('./template/navbar.php'); 
+
+?>
+<div class="container pt-3">
+    <h3>Login</h3>
+</div>
+<div class="m-3">
+<?=$email?>
+<div class="card flex m-5 p-5 bg-secondary bg-opacity-10">
+<form method="post" class="">
+<div class="form-group">
+    <label for="exampleInputEmail1">E-Mail address</label>
+    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Login</button>
+  <?php
+    if ($loginFailed) {
+  ?>
+      <a href="register.php" class="btn btn-secondary">Add profile</a>
+  <?php
+    }
+  ?>
+</div>
+</form>
+</div>
+<?php include('./template/footer.php'); ?>
